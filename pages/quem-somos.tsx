43da2f467cca41a7c809ca003bc70f3cb46295f9
eduardo <eduardoaleixomartins@gmail.com>
2019-08-14 16:26:18 -0300

    refactor(quem-somos): comply with pr suggestions
    * use h3 to style, but make the component h1
    * don't set 'variant="body1"' since it's the default
    * remove the wrapping div used to center the action button

--- pages/quem-somos.tsx
+++ pages/quem-somos.tsx
@@ -1,15 +1,13 @@
-import { Card, Marker, Navbar } from '../components';
+import { Navbar } from '../components';
 import {
   Container,
   Typography,
-  Paper,
   makeStyles,
   Theme,
   createStyles,
   Button,
-  Fab,
-  Link,
 } from '@material-ui/core';
+import Link from 'next/link';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,9 +18,11 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       paddingBottom: `${theme.spacing(2)}px`,
     },
-    buttonWrapper: {
-      paddingTop: `${theme.spacing(4)}px`,
-      textAlign: 'center',
+    button: {
+      marginTop: `${theme.spacing(5)}px`,
+
+      margin: '0 auto',
+      display: 'block',
     },
   })
 );
@@ -33,31 +33,36 @@ export default function AboutUs() {
     <>
       <Navbar />
       <Container className={classes.container} maxWidth="md">
-        <Typography variant="h3" gutterBottom className={classes.title}>
+        <Typography
+          component="h1"
+          variant="h3"
+          gutterBottom
+          className={classes.title}
+        >
           Quem somos
         </Typography>
 
-        <Typography variant="body1" gutterBottom paragraph>
-          O Comer Muda o Mundo não é um manual de instruções, um guia culinário
-          ou mesmo um mapa de caça ao tesouro. Esse é um canal de boas novas. É
-          um local para nos enchermos de esperança. É, sobretudo, um espaço para
-          promover encontros.
+        <Typography gutterBottom paragraph>
+          O <strong>Comer Muda o Mundo</strong> não é um manual de instruções,
+          um guia culinário ou mesmo um mapa de caça ao tesouro. Esse é um canal
+          de boas novas. É um local para nos enchermos de esperança. É,
+          sobretudo, um espaço para promover encontros.
         </Typography>
 
-        <Typography variant="body1" gutterBottom paragraph>
+        <Typography gutterBottom paragraph>
           Sabe aquele ditado “quem procura acha”? Aqui, nós achamos. Achamos
           quem, como a gente, quer fazer do ato comer uma forma de mudar o
           mundo.
         </Typography>
 
-        <Typography variant="body1" gutterBottom paragraph>
+        <Typography gutterBottom paragraph>
           Por aqui você vai encontrar uma lista de locais que não são
           necessariamente restaurantes, supermercados ou mercearias. Não estamos
           falando de orgânicos de grife ou daqueles alimentos “saudáveis” em
           bandeja de isopor com plástico filme.
         </Typography>
 
-        <Typography variant="body1" gutterBottom paragraph>
+        <Typography gutterBottom paragraph>
           O nosso papo é sobre mais do que isso. Aqui há um apanhado de espaços
           para comprar, cozinhar ou comer comida boa, limpa e justa. Neles,
           você, com certeza, pode descobrir de onde a sua comida vem, perguntar
@@ -66,17 +71,22 @@ export default function AboutUs() {
           pessoa que a está comercializando e especialmente do planeta.
         </Typography>
 
-        <Typography variant="body1" gutterBottom paragraph>
+        <Typography gutterBottom paragraph>
           Navegue pelas nossas páginas, para depois sair do mundo virtual e
           promover encontros reais em locais que envolvam essa combinação
           poderosa: comida de verdade com pessoas de verdade.
         </Typography>
 
-        <div className={classes.buttonWrapper}>
-          <Button variant="outlined" color="primary" size="large">
-            <Link href="/">Vem com a gente?</Link>
+        <Link href="/">
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            className={classes.button}
+          >
+            Vem com a gente?
           </Button>
-        </div>
+        </Link>
       </Container>
     </>
   );
