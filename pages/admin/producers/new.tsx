88a889023a0866a0620e3a7d8cf37f1d10bde3e8
Daniela Yassuda <daniela.yassuda@quintoandar.com.br>
2019-08-14 00:07:49 -0300

    Create producer

--- pages/admin/producers/new.tsx
+++ pages/admin/producers/new.tsx
@@ -1,16 +1,19 @@
-import { Navbar, LoadingButton, ErrorSnackbar } from '@components/index';
-import { Container, TextField, Theme, Grid } from '@material-ui/core';
-import { useState, ChangeEvent, FormEvent } from 'react';
-import { makeStyles, createStyles } from '@material-ui/styles';
-import * as ProducerAPI from '@api/producer';
-import Router from 'next/router';
-import './new.css';
-import { SearchBar } from './_SearchBar';
-import * as yup from 'yup';
+import { Grid, TextField, Typography } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { Formik, FormikTouched, FormikErrors } from 'formik';
-import withAuth from '@app/containers/withAuth';
+import Head from 'next/head';
+import Router from 'next/router';
+import * as yup from 'yup';
 
-export const NewProducerSchema = yup.object({
+import * as ProducerAPI from '@api/producer';
+import AdminPage from '@components/AdminPage';
+import Breadcrumbs from '@components/Breadcrumbs';
+import LoadingButton from '@components/LoadingButton';
+import SearchBar from '@containers/SearchBar';
+import withAuth from '@containers/withAuth';
+import '@styles/search-bar.css';
+
+const NewProducerSchema = yup.object({
   name: yup.string().required(),
   address: yup.string().required(),
 
@@ -28,6 +31,7 @@ export const NewProducerSchema = yup.object({
     .max(-29)
     .min(-74),
 });
+
 type NewProducer = yup.InferType<typeof NewProducerSchema>;
 
 const useStyles = makeStyles(() =>
@@ -49,137 +53,6 @@ const useStyles = makeStyles(() =>
   })
 );
 
-function NewProducer() {
-  console.log('in new producer');
-  const classes = useStyles();
-
-  return (
-    <>
-      <Navbar admin />
-      <Container>
-        <Formik
-          initialValues={{
-            name: '',
-            address: '',
-            latitude: -9999,
-            longitude: -9999,
-          }}
-          validationSchema={NewProducerSchema}
-          onSubmit={(newProducer: NewProducer) => {
-            return ProducerAPI.create(newProducer).then(res => {
-              Router.push('/admin/producers');
-            });
-          }}
-        >
-          {({
-            values,
-            handleChange,
-            errors,
-            touched,
-            handleSubmit,
-            isSubmitting,
-            setSubmitting,
-            setFieldValue,
-          }) => (
-            <form
-              onSubmit={async r => {
-                await handleSubmit(r);
-
-                setSubmitting(false);
-              }}
-              noValidate
-              autoComplete="off"
-              className={classes.container}
-            >
-              <TextField
-                id="input-name"
-                name="name"
-                label="Nome"
-                required
-                value={values.name}
-                error={!!(touched.name && errors.name)}
-                onChange={handleChange}
-                margin="normal"
-                helperText={mountErrMsg(touched, errors, 'name')}
-                fullWidth
-              />
-
-              <div className="search-bar-wrapper">
-                <SearchBar
-                  onPlaceChanged={v => {
-                    setFieldValue('latitude', v.latitude);
-                    setFieldValue('longitude', v.longitude);
-                    setFieldValue('address', v.address);
-                  }}
-                ></SearchBar>
-              </div>
-
-              <TextField
-                id="address"
-                label="Endereço Amigável"
-                value={values.address}
-                required
-                onChange={handleChange}
-                error={!!(touched.address && errors.address)}
-                margin="normal"
-                helperText={mountErrMsg(
-                  touched,
-                  errors,
-                  'address',
-                  'Que será mostrado ao usuário'
-                )}
-                fullWidth
-              />
-
-              <Grid container spacing={2}>
-                <Grid item xs={6}>
-                  <TextField
-                    id="input-latitude"
-                    label="Latitude"
-                    value={values.latitude}
-                    error={!!(touched.latitude && errors.latitude)}
-                    helperText={mountErrMsg(touched, errors, 'latitude')}
-                    required
-                    margin="normal"
-                    disabled
-                    fullWidth
-                  />
-                </Grid>
-
-                <Grid item xs={6}>
-                  <TextField
-                    id="input-longitude"
-                    label="Longitude"
-                    value={values.longitude}
-                    error={!!(touched.longitude && errors.longitude)}
-                    helperText={mountErrMsg(touched, errors, 'longitude')}
-                    required
-                    margin="normal"
-                    disabled
-                    fullWidth
-                  />
-                </Grid>
-              </Grid>
-
-              <LoadingButton
-                loading={isSubmitting}
-                type="submit"
-                fullWidth
-                color="primary"
-                variant="contained"
-              >
-                Salvar
-              </LoadingButton>
-            </form>
-          )}
-        </Formik>
-      </Container>
-    </>
-  );
-}
-
-export default withAuth(NewProducer);
-
 function mountErrMsg(
   touched: FormikTouched<NewProducer>,
   errors: FormikErrors<NewProducer>,
@@ -195,3 +68,151 @@ function mountErrMsg(
 
   return defaultMsg;
 }
+
+function NewProducer() {
+  const classes = useStyles();
+
+  return (
+    <AdminPage>
+      <Head>
+        <title>Nova iniciativa - Comer muda o mundo</title>
+      </Head>
+
+      <Breadcrumbs
+        aria-label="Breadcrumb"
+        items={[
+          { href: '/admin', label: 'Admin' },
+          { href: '/admin/producers', label: 'Iniciativas' },
+          { href: '/admin/producers/new', label: 'Nova iniciativa' },
+        ]}
+      />
+
+      <Typography component="h1" variant="h4" gutterBottom>
+        Nova iniciativa
+      </Typography>
+
+      <Formik
+        initialValues={{
+          name: '',
+          address: '',
+          latitude: -9999,
+          longitude: -9999,
+        }}
+        validationSchema={NewProducerSchema}
+        onSubmit={(newProducer: NewProducer) => {
+          return ProducerAPI.create(newProducer).then(res => {
+            Router.push('/admin/producers');
+          });
+        }}
+      >
+        {({
+          values,
+          handleChange,
+          errors,
+          touched,
+          handleSubmit,
+          isSubmitting,
+          setSubmitting,
+          setFieldValue,
+        }) => (
+          <form
+            onSubmit={async r => {
+              await handleSubmit(r);
+
+              setSubmitting(false);
+            }}
+            noValidate
+            autoComplete="off"
+            className={classes.container}
+          >
+            <TextField
+              id="input-name"
+              name="name"
+              label="Nome"
+              margin="normal"
+              variant="outlined"
+              value={values.name}
+              error={!!(touched.name && errors.name)}
+              onChange={handleChange}
+              helperText={mountErrMsg(touched, errors, 'name')}
+              fullWidth
+              required
+            />
+
+            <div className="search-bar-wrapper">
+              <SearchBar
+                onPlaceChanged={v => {
+                  setFieldValue('latitude', v.latitude);
+                  setFieldValue('longitude', v.longitude);
+                  setFieldValue('address', v.address);
+                }}
+              ></SearchBar>
+            </div>
+
+            <TextField
+              id="address"
+              label="Endereço Amigável"
+              margin="normal"
+              variant="outlined"
+              value={values.address}
+              onChange={handleChange}
+              error={!!(touched.address && errors.address)}
+              helperText={mountErrMsg(
+                touched,
+                errors,
+                'address',
+                'Que será mostrado ao usuário'
+              )}
+              fullWidth
+              required
+            />
+
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <TextField
+                  id="input-latitude"
+                  margin="normal"
+                  label="Latitude"
+                  variant="outlined"
+                  value={values.latitude}
+                  error={!!(touched.latitude && errors.latitude)}
+                  helperText={mountErrMsg(touched, errors, 'latitude')}
+                  disabled
+                  fullWidth
+                  required
+                />
+              </Grid>
+
+              <Grid item xs={6}>
+                <TextField
+                  id="input-longitude"
+                  label="Longitude"
+                  margin="normal"
+                  variant="outlined"
+                  value={values.longitude}
+                  error={!!(touched.longitude && errors.longitude)}
+                  helperText={mountErrMsg(touched, errors, 'longitude')}
+                  disabled
+                  fullWidth
+                  required
+                />
+              </Grid>
+            </Grid>
+
+            <LoadingButton
+              color="primary"
+              type="submit"
+              variant="contained"
+              loading={isSubmitting}
+              fullWidth
+            >
+              Salvar
+            </LoadingButton>
+          </form>
+        )}
+      </Formik>
+    </AdminPage>
+  );
+}
+
+export default withAuth(NewProducer);
