68cbc40144a4b5001a4e5505dda96ba3ba64abd7
eduardo <eduardoaleixomartins@gmail.com>
2019-10-12 00:38:00 -0300

    build: refactor file structure to add nestjs

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, All } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @All() // Matches "*" on all methods GET, POST...
+  genericFunction() {
+    return '404 page';
   }
 }
