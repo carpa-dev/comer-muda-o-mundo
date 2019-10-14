fd9205106934409441fe34a1baa2f349cd988be3
eduardo <eduardoaleixomartins@gmail.com>
2019-10-14 13:13:42 -0300

    build

--- nestjs/ormconfig.js
+++ nestjs/ormconfig.js
@@ -3,9 +3,8 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['migrations/*.js'],
-  migrationsRun: true,
   retryAttempts: 1,
   extra: {
     ssl: !!process.env.DATABASE_SSL,
