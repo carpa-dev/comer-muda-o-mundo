dd90ea5b9e5350098fc22db5754281230195b338
Daniela Sayuri Yassuda <danielayassuda@gmail.com>
2020-09-07 22:29:18 -0300

    fix: cms container and toolbar position

--- pages/cms.tsx
+++ pages/cms.tsx
@@ -1,8 +1,18 @@
 import { useEffect } from 'react';
 
+declare global {
+  interface Window {
+    CMS_MANUAL_INIT: boolean | undefined;
+  }
+}
+
+// Pre-create root container to have more control over styles
+// https://github.com/netlify/netlify-cms/blob/e73f38d5c260fd70cda38a3e842e21b99002503b/packages/netlify-cms-core/src/bootstrap.js#L19
+const NETLIFY_CMS_ROOT_ELEMENT_ID = 'nc-root';
+
 export default function Cms() {
   useEffect(() => {
-    (window as any).CMS_MANUAL_INIT = true;
+    window.CMS_MANUAL_INIT = true;
 
     // TODO:
     // abstract this into a config object
@@ -69,5 +79,11 @@ export default function Cms() {
     });
   }, []);
 
-  return null;
+  return (
+    <div className="w-full h-full pt-12">
+      <div className="w-full h-full relative">
+        <div id={NETLIFY_CMS_ROOT_ELEMENT_ID}></div>
+      </div>
+    </div>
+  );
 }
